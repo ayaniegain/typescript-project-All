c4fa01eaa324ec95db18ebd1c4f28b9881c50186
ayaniegain <ayaniegain@gmail.com>
2023-10-08 07:30:04 +0530

    feat:2nd content

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,9 @@
 "use strict";
+// -------classes--------
+//type assertion
+// const btn= document.getElementById('btn') as HTMLElement
+// const btn= <HTMLElement>document.getElementById('btn');
+const btn = document.getElementById('btn');
+btn.onclick;
+const img = document.getElementById('myimg');
+img.src;
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// -------classes--------
+
+//type assertion
+
+// const btn= document.getElementById('btn') as HTMLElement
+// const btn= <HTMLElement>document.getElementById('btn');
+const btn= document.getElementById('btn')!;
+btn.onclick
+
+const img= document.getElementById('myimg') as HTMLImageElement;
+img.src
